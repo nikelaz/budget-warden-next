2288b8e8ab293d388662cdfeed6fa93cd638d558
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2026-05-07 21:10:15 +0300

    feat: additional unit tests

--- macos/BudgetWardenTests/MoneyAmountTests.swift
+++ macos/BudgetWardenTests/MoneyAmountTests.swift
@@ -8,6 +8,7 @@ final class MoneyAmountTests: XCTestCase {
         XCTAssertEqual(UInt64.parseMoneyAmount("12.34"), 1_234)
         XCTAssertEqual(UInt64.parseMoneyAmount("0,99"), 99)
         XCTAssertEqual(UInt64.parseMoneyAmount(".50"), 50)
+        XCTAssertEqual(UInt64.parseMoneyAmount(" 1234 "), 123_400)
     }
 
     func testParseMoneyAmountRejectsInvalidValues() {
@@ -15,5 +16,18 @@ final class MoneyAmountTests: XCTestCase {
         XCTAssertNil(UInt64.parseMoneyAmount("12.3.4"))
         XCTAssertNil(UInt64.parseMoneyAmount("budget"))
         XCTAssertNil(UInt64.parseMoneyAmount("-1"))
+        XCTAssertNil(UInt64.parseMoneyAmount("1 2"))
+        XCTAssertNil(UInt64.parseMoneyAmount("1."))
+    }
+
+    func testParseMoneyAmountUsesEmptyValueForBlankInput() {
+        XCTAssertEqual(UInt64.parseMoneyAmount("", emptyValue: 0), 0)
+        XCTAssertEqual(UInt64.parseMoneyAmount(" \n ", emptyValue: 12_345), 12_345)
+        XCTAssertNil(UInt64.parseMoneyAmount(""))
+    }
+
+    func testParseMoneyAmountRejectsOverflow() {
+        XCTAssertNil(UInt64.parseMoneyAmount("184467440737095517"))
+        XCTAssertNil(UInt64.parseMoneyAmount("184467440737095516.16"))
     }
 }
