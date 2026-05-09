67cfd4e4ecd3f876daca921ebbd1dbf19acb8a69
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2026-05-09 20:15:09 +0300

    chore: add file header comments

--- core/src/bw-date.h
+++ core/src/bw-date.h
@@ -1,3 +1,13 @@
+/* 
+ * Budget Warden Core
+ * Copyright (c) 2026 Lazarov & Co EOOD
+ * Author: Nikola Lazarov
+ *
+ * Licensed under the Source-Available Educational License. 
+ * See the LICENSE file in the project root for full terms.
+ *
+ */
+
 #ifndef BW_DATE_H
 #define BW_DATE_H
 
--- macos/BudgetWarden/Views/Budget/BudgetRowView.swift
+++ macos/BudgetWarden/Views/Budget/BudgetRowView.swift
@@ -1,3 +1,13 @@
+/* 
+ * Budget Warden Core
+ * Copyright (c) 2026 Lazarov & Co EOOD
+ * Author: Nikola Lazarov
+ *
+ * Licensed under the Source-Available Educational License. 
+ * See the LICENSE file in the project root for full terms.
+ *
+ */
+
 import SwiftUI
 
 struct BudgetRowView: View {
