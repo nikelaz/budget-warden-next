07d7b4038b9385e9cce94ab069f2ee6c3620fbd6
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2026-05-11 10:42:46 +0300

    chore: header in store

--- macos/BudgetWarden/ViewModels/BWStore.swift
+++ macos/BudgetWarden/ViewModels/BWStore.swift
@@ -1,3 +1,13 @@
+/*
+ * Budget Warden
+ * Copyright (c) 2026 Lazarov & Co EOOD
+ * Author: Nikola Lazarov
+ *
+ * Licensed under the Source-Available Educational License.
+ * See the LICENSE file in the project root for full terms.
+ *
+ */
+
 import Foundation
 import Combine
 
